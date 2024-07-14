a16789639879d1651034c66f6612f49a626bc584
Anna Nikulina <anna.nikulina1995@yandex.ru>
2024-07-15 00:12:45 +0300

    fixed bugs after first iteration of code review

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,3 +1,5 @@
+import { ArticleState, articleDefaultPageState } from '../app/App';
+import { Dispatch, SetStateAction, useRef, useState } from 'react';
 import {
 	OptionType,
 	backgroundColors,
@@ -6,7 +8,6 @@ import {
 	fontFamilyOptions,
 	fontSizeOptions,
 } from '../../constants/articleProps';
-import { useRef, useState } from 'react';
 
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
@@ -18,108 +19,153 @@ import cn from 'classnames';
 import styles from './ArticleParamsForm.module.scss';
 import { useClickOutside } from './hooks/useClickOutside';
 
+export type ArticleFormState = {
+	fontFamily: {
+		title: string;
+		value: string;
+		className: string;
+	};
+	fontSize: {
+		title: string;
+		value: string;
+		className: string;
+	};
+	fontColor: {
+		title: string;
+		value: string;
+		className: string;
+	};
+	backgroundColor: {
+		title: string;
+		value: string;
+		className: string;
+	};
+	contentWidth: {
+		title: string;
+		value: string;
+		className: string;
+	};
+};
+
 type ArticleParamsFormProps = {
-	states: {
-		fontFamily: {
-			title: string;
-			value: string;
-			className: string;
-		};
-		fontSize: {
-			title: string;
-			value: string;
-			className: string;
-		};
-		fontColor: {
-			title: string;
-			value: string;
-			className: string;
-		};
-		backgroundColor: {
-			title: string;
-			value: string;
-			className: string;
-		};
-		contentWidth: {
-			title: string;
-			value: string;
-			className: string;
-		};
-	};
-	handlers: {
-		onChangeFontFamily: (value: OptionType) => void;
-		onChangeFontSize: (value: OptionType) => void;
-		onChangeFontColor: (value: OptionType) => void;
-		onChangeBackgroundColor: (value: OptionType) => void;
-		onChangeContentWidth: (value: OptionType) => void;
-	};
-	submit: () => void;
-	reset: () => void;
+	changePageState: Dispatch<SetStateAction<ArticleState>>;
 };
 
 export const ArticleParamsForm = (props: ArticleParamsFormProps) => {
-	const [isOpen, setIsOpen] = useState<boolean>(false);
+	const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
 	const rootRef = useRef<HTMLDivElement>(null);
 
 	const isClicked = () => {
-		setIsOpen(!isOpen);
+		setIsMenuOpen(!isMenuOpen);
 	};
 
-	useClickOutside(rootRef, setIsOpen);
+	useClickOutside(rootRef, setIsMenuOpen);
+
+	const articleDefaultFormState: ArticleFormState = {
+		fontFamily: { title: '', value: '', className: '' },
+		fontSize: { title: '', value: '', className: '' },
+		fontColor: { title: '', value: '', className: '' },
+		backgroundColor: { title: '', value: '', className: '' },
+		contentWidth: { title: '', value: '', className: '' },
+	};
+
+	const [formState, setFormState] = useState(articleDefaultFormState);
+
+	const handlers = {
+		onChangeFontFamily: (value: OptionType) => {
+			setFormState({ ...formState, fontFamily: value });
+		},
+		onChangeFontSize: (value: OptionType) => {
+			setFormState({ ...formState, fontSize: value });
+		},
+		onChangeFontColor: (value: OptionType) => {
+			setFormState({ ...formState, fontColor: value });
+		},
+		onChangeBackgroundColor: (value: OptionType) => {
+			setFormState({ ...formState, backgroundColor: value });
+		},
+		onChangeContentWidth: (value: OptionType) => {
+			setFormState({ ...formState, contentWidth: value });
+		},
+	};
+
+	const articleNewPageState: ArticleState = {
+		fontFamily: formState.fontFamily.value,
+		fontSize: formState.fontSize.value,
+		fontColor: formState.fontColor.value,
+		backgroundColor: formState.backgroundColor.value,
+		contentWidth: formState.contentWidth.value,
+	};
+
+	const handleSubmit = (
+		setPageState: Dispatch<SetStateAction<ArticleState>>
+	) => {
+		setPageState(articleNewPageState);
+	};
+
+	const handleReset = (
+		setPageState: Dispatch<SetStateAction<ArticleState>>
+	) => {
+		setPageState(articleDefaultPageState);
+		setFormState(articleDefaultFormState);
+	};
 
 	return (
 		<>
 			<div ref={rootRef}>
-				<ArrowButton onClick={isClicked} state={isOpen} />
+				<ArrowButton onClick={isClicked} state={isMenuOpen} />
 				<aside
-					className={cn(styles.container, isOpen ? styles.container_open : '')}>
+					className={cn(
+						styles.container,
+						isMenuOpen ? styles.container_open : ''
+					)}>
 					<form
 						className={styles.form}
 						onSubmit={(e) => {
 							e.preventDefault();
-							props.submit();
+							handleSubmit(props.changePageState);
 						}}
 						onReset={(e) => {
 							e.preventDefault();
-							props.reset();
+							handleReset(props.changePageState);
 						}}>
 						<Text size={31} weight={800} uppercase={true}>
 							Задайте параметры
 						</Text>
 						<Select
 							options={fontFamilyOptions}
-							selected={props.states.fontFamily}
+							selected={formState.fontFamily}
 							title='Шрифт'
-							onChange={props.handlers.onChangeFontFamily}
+							onChange={handlers.onChangeFontFamily}
 							placeholder='Выберите шрифт'
 						/>
 						<Select
 							options={fontColors}
-							selected={props.states.fontColor}
+							selected={formState.fontColor}
 							title='Цвет шрифта'
-							onChange={props.handlers.onChangeFontColor}
+							onChange={handlers.onChangeFontColor}
 							placeholder='Выберите цвет шрифта'
 						/>
 						<RadioGroup
 							name='Размер шрифта'
 							title='Размер шрифта'
 							options={fontSizeOptions}
-							selected={props.states.fontSize}
-							onChange={props.handlers.onChangeFontSize}
+							selected={formState.fontSize}
+							onChange={handlers.onChangeFontSize}
 						/>
 						<Separator />
 						<Select
 							options={backgroundColors}
-							selected={props.states.backgroundColor}
+							selected={formState.backgroundColor}
 							title='Цвет фона'
-							onChange={props.handlers.onChangeBackgroundColor}
+							onChange={handlers.onChangeBackgroundColor}
 							placeholder='Выберите цвет фона'
 						/>
 						<Select
 							options={contentWidthArr}
-							selected={props.states.contentWidth}
+							selected={formState.contentWidth}
 							title='Ширина контента'
-							onChange={props.handlers.onChangeContentWidth}
+							onChange={handlers.onChangeContentWidth}
 							placeholder='Выберите ширину контента'
 						/>
 						<div className={styles.bottomContainer}>
--- src/index.tsx
+++ src/index.tsx
@@ -1,94 +1,11 @@
 import './styles/index.scss';
 
-import { CSSProperties, StrictMode, useState } from 'react';
-
-import { Article } from './components/article/Article';
-import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { OptionType, defaultArticleState } from './constants/articleProps';
-import clsx from 'clsx';
+import { App } from './components/app/App';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
-import styles from './styles/index.module.scss';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
-
-const App = () => {
-	const articleFormState = {
-		fontFamily: { title: '', value: '', className: '' },
-		fontSize: { title: '', value: '', className: '' },
-		fontColor: { title: '', value: '', className: '' },
-		backgroundColor: { title: '', value: '', className: '' },
-		contentWidth: { title: '', value: '', className: '' },
-	};
-
-	const [formState, setFormState] = useState(articleFormState);
-
-	const handlers = {
-		onChangeFontFamily: (value: OptionType) => {
-			setFormState({ ...formState, fontFamily: value });
-		},
-		onChangeFontSize: (value: OptionType) => {
-			setFormState({ ...formState, fontSize: value });
-		},
-		onChangeFontColor: (value: OptionType) => {
-			setFormState({ ...formState, fontColor: value });
-		},
-		onChangeBackgroundColor: (value: OptionType) => {
-			setFormState({ ...formState, backgroundColor: value });
-		},
-		onChangeContentWidth: (value: OptionType) => {
-			setFormState({ ...formState, contentWidth: value });
-		},
-	};
-
-	const articleState = {
-		fontFamily: defaultArticleState.fontFamilyOption.value,
-		fontSize: defaultArticleState.fontSizeOption.value,
-		fontColor: defaultArticleState.fontColor.value,
-		backgroundColor: defaultArticleState.contentWidth.value,
-		contentWidth: defaultArticleState.backgroundColor.value,
-	};
-
-	const [pageState, setPageState] = useState(articleState);
-
-	const handleSubmit = () => {
-		setPageState({
-			fontFamily: formState.fontFamily.value,
-			fontSize: formState.fontSize.value,
-			fontColor: formState.fontColor.value,
-			backgroundColor: formState.backgroundColor.value,
-			contentWidth: formState.contentWidth.value,
-		});
-	};
-
-	const handleReset = () => {
-		setPageState(articleState);
-		setFormState(articleFormState);
-	};
-
-	return (
-		<div
-			className={clsx(styles.main)}
-			style={
-				{
-					'--font-family': pageState.fontFamily,
-					'--font-size': pageState.fontSize,
-					'--font-color': pageState.fontColor,
-					'--container-width': pageState.contentWidth,
-					'--bg-color': pageState.backgroundColor,
-				} as CSSProperties
-			}>
-			<ArticleParamsForm
-				states={formState}
-				handlers={handlers}
-				submit={handleSubmit}
-				reset={handleReset}
-			/>
-			<Article />
-		</div>
-	);
-};
 
 root.render(
 	<StrictMode>
